5e09724cb495c8af0a93a1a6d53c1ad91c820d23
fajaraulia <fajaarauliiaa@gmail.com>
2023-05-11 16:15:12 +0700

    first-commit

--- Backend Laravel/database/seeders/SudokuSeeder.php
+++ Backend Laravel/database/seeders/SudokuSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,13 @@ class SudokuSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'name' => "Level 1",
+            'file_name' => "level_1.txt",
+        ]);
+        DB::table('users')->insert([
+            'name' => "Level 2",
+            'file_name' => "level_2.txt",
+        ]);
     }
 }
